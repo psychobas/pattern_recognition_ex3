1caf88da71268f6b30b9185fd1096ce63096e03e
Lars Waldvogel <lars.waldvogel@stud.unibas.ch>
2020-11-01 00:24:35 +0100

    fixed biggest bugs after last commit, still not working

--- code/logreg.py
+++ code/logreg.py
@@ -6,6 +6,7 @@ class LOGREG(object):
     Logistic regression class based on the LOGREG lecture slides
     '''
 
+
     def __init__(self, regularization: float = 0):
         self.r = regularization
         self._threshold = 10e-9
@@ -65,17 +66,36 @@ class LOGREG(object):
         :return: first derivative of the model parameters
         '''
         # TODO: Calculate derivative of loglikelihood function for posterior p(y=1|X,w) -> DONE, CHECKED
-
+        DEBUG = False
         h = self.activationFunction(w, X)
-
+        numOfEntries = X.shape[1]
+        dim = X.shape[0]
         # print("shape of X is: ", X.shape)
         # print("shape of h is: ", h.shape)
         # print("shape of y is: ", y.shape)
         # print("shape of reshaped x is: ", X.shape)
 
-        # firstDerivative = np.dot(X, (h.flatten() - y.flatten())) / y.shape[0] # I do not know what formula you are
-        # using, but take a look at slide 20
-        firstDerivative = (y - h) * X.T  # unsure if a sum is needed here or a specific index of x and y
+        #firstDerivative = np.dot(X, (h.flatten() - y.flatten())) / y.shape[0] # I do not know what formula you are
+                                                                                # using, but take a look at slide 20
+        firstDerivative = np.zeros(dim)
+        for i in range(numOfEntries):
+            x_i = X[:, i]
+            y_i = y[i]
+            h_i = h[:,i]
+            if DEBUG:
+                print(np.shape(X))
+                print(np.shape(y))
+                print(np.shape(h))
+                print(np.shape(x_i))
+                print(np.shape(y_i))
+                print(np.shape(h_i))
+                print(np.shape(y_i-h_i))
+                print(np.shape((y_i-h_i)*x_i.T))
+                print(np.shape(firstDerivative[:,i]))
+            firstDerivative += (y_i-h_i)*x_i.T
+
+        # firstDerivative = np.sum((y - h) * X.T)  # not working
+
         # print("h flattened is: ", h.flatten())
         # print("y flattened is: ", y.flatten())
         # print("first Derivative is: ", firstDerivative)
@@ -105,8 +125,10 @@ class LOGREG(object):
 
         # hessian = X.T.dot(w).dot(X);
         # https://stackoverflow.com/questions/58567344/python-logistic-regression-hessian-getting-a-divide-by-zero-error-and-a-singu check!
-        # hessian = (np.dot(X, X.T) * np.diag(h) * np.diag(1 - h)) # not sure why you are using diagonals here
-        hessian = - np.sum((np.dot(X, X.T) * h * (1 - h)))  # slide 22
+        hessian = -(np.dot(X, X.T) * np.diag(h) * np.diag(1 - h)) # not sure why you are using diagonals here
+
+        #hessian = - np.sum((np.dot(X, X.T) * (h * (1 - h)))  # slide 22, does not work due to mismatched dimensions
+                                                              # trusting your formula instead
 
         # print("hessian is: ", hessian)
         # print("shape of X is: ", X.shape)
@@ -154,7 +176,7 @@ class LOGREG(object):
             if self.r == 0:
                 # TODO: What happens if this condition is removed?
                 #  -> we get a singular matrix error when computing the dot product for the w_update DONE, CHECKED
-                if np.exp(posteriorloglikelihood) > 1 - self._eps:
+                if np.any(np.exp(posteriorloglikelihood) > 1 - self._eps):
                     print('posterior > 1-eps, breaking optimization at niter = ', i)
                     break
 
